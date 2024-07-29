4a4025e338563da3a09a9b3b566895e38a579e74
Detlef Groth <dgroth@uni-potsdam.de>
2024-07-29 10:01:53 +0200

    fixing verbose issues

--- templates/app-argparse2.cpp
+++ templates/app-argparse2.cpp
@@ -5,7 +5,7 @@
 #include "include/argparse.hpp"
 
 static const char USAGE[] =
-R"(Usage: APPNAME [-h,--help -v,--verbose -V,--version] --round n number
+R"(Usage: APPNAME [-h,--help -V,--verbose -v,--version -s,--say-hello] --round n number
 )";
 
 int main(int argc, char *argv[]) {
@@ -14,30 +14,29 @@ int main(int argc, char *argv[]) {
               << std::endl;
         return(0);
     }
-    argparse::ArgumentParser program("argparse");
-    int x;      // decimal points    double y;   // float to be rounded 
-    double number ;
+    argparse::ArgumentParser program("argparse","0.0.1");
+    int x = 2;      // decimal points    double y;   // float to be rounded 
+    double number = 0.0;
+    bool hello = false;
     bool verbose = false;
-    bool version = false;
     // positional argument no leading dash
     program.add_argument("number")
           .help("display the square of this given number")
           .metavar("NUMBER")
           .scan<'f',float>().store_into(number);
     // option with parameter -leading dash
-    program.add_argument("--round").help("round a number").metavar("POINTS")
+    program.add_argument("-r","--round").help("round a number").metavar("POINTS")
                 .choices(0,1,2,3,4,5,6,7,8,9).store_into(x); 
     // flag
-    program.add_argument("-v", "--verbose")
-                .help("set verbose on")
-                .default_value(false)
-                .implicit_value(true)
-                .store_into(verbose);
-    program.add_argument("-V", "--version")
-          .help("show version information")
-          .default_value(false)
-          .implicit_value(true)
-          .action([&](const auto &){ std::cout << "0.0.1\n"; exit(0); });
+    program.add_argument("-V", "--verbose")
+                .help("set verbose on").flag().store_into(verbose);
+    // action example
+    program.add_argument("-s", "--say-hello")
+          .help("Say Hello World!")
+          .flag()
+          .store_into(hello)
+          .action([&](const auto &){ std::cout << "Hello World!\n"; exit(0); });
+    
     try {
         program.parse_args(argc, argv);
     }
@@ -47,7 +46,7 @@ int main(int argc, char *argv[]) {
               << std::endl;
         exit(0);
     }
-    if (verbose) { std::cout << "verbose is on\n"; }
+    if (program["--verbose"] == true) { std::cout << "verbose is on\n"; }
     std::cout << "square of: " << number << " is " <<
           std::fixed << std::setprecision(x) <<
           number*number <<  std::endl;
